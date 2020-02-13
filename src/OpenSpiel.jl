119d1d56b8904446d2148f2765f772e436fc17a7
Jun Tian <tianjun.cpp@gmail.com>
2020-02-13 19:22:41 +0800

    init commit

--- src/OpenSpiel.jl
+++ src/OpenSpiel.jl
@@ -1,5 +1,30 @@
+__precompile__(false)
 module OpenSpiel
 
-greet() = print("Hello World!")
+using CxxWrap
+using OpenSpiel_jll
+
+import CxxWrap:argument_overloads
+import Base:show, length, getindex, setindex!, keys, values, copy, deepcopy, first, last, step, getfield, setfield!
+
+@wrapmodule(libspieljl)
+
+include("patch.jl")
+
+# export all
+for n in names(@__MODULE__(); all=true)
+    if Base.isidentifier(n) &&
+        !startswith(String(n), "_") &&
+        n ∉ (Symbol(@__MODULE__()), :eval, :include)
+        @eval export $n
+    end
+end
+
+
+function __init__()
+    # TODO: https://github.com/JuliaInterop/libcxxwrap-julia/issues/39#issuecomment-585019888
+    # @initcxx
+end
+
 
 end # module
