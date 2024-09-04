a041e5aaa9eafc149fa5a21e5aa2447f5672fa0c
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-09-04 22:41:29 +0200

    try fixing game load

--- src/patch.jl
+++ src/patch.jl
@@ -60,19 +60,19 @@ end
 
 function load_game(s::Union{String, CxxWrap.StdLib.StdStringAllocated}; kw...)
     if length(kw) == 0
-        _load_game(s)
+        _load_game(s)[]
     else
         ps = [StdString(string(k)) => v for (k,v) in kw]
-        _load_game(s, StdMap{StdString, GameParameter}(ps))
+        _load_game(s, StdMap{StdString, GameParameter}(ps))[]
     end
 end
 
 function load_game_as_turn_based(s::Union{String, CxxWrap.StdLib.StdStringAllocated}; kw...)
     if length(kw) == 0
-        _load_game_as_turn_based(s)
+        _load_game_as_turn_based(s)[]
     else
         ps = [StdString(string(k)) => v for (k,v) in kw]
-        _load_game_as_turn_based(s, StdMap{StdString, GameParameter}(ps))
+        _load_game_as_turn_based(s, StdMap{StdString, GameParameter}(ps))[]
     end
 end
 
